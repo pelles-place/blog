9660a6a63606b074c4879839ab32c4f52bd3687c
pelle <55886471+maxpellnitz@users.noreply.github.com>
2024-04-30 11:14:56 +0200

    Add profil-model, tag-model and post-model (#1)
    * Add profil-model, tag-model and post-model
    
    * Clean up code
    
    ---------
    
    Co-authored-by: Maximilian Pellnitz <max.pellnitz@posteo.com>

---
+++ backend/blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT )
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=255, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.get_username()
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True) #max_length must be defined
+    subtitle = models.CharField(max_length=255, blank=True) #null=True not necessary b/c django CharField saves empty String
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_description = models.CharField(max_length=150, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models. DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ["-publish_date"]  #order by publish_date in descending order
